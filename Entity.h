4b9f60a5e2b0d8635cc079c28314014d99b5a4ed
daoxunchen <acy631545607@gmail.com>
2016-04-30 15:22:52 +0800

    minus changes in basic type

--- Entity.h
+++ Entity.h
@@ -6,14 +6,14 @@ typedef __int64 Id_type;
 
 struct AA
 {
-	Id_type Auid;
-	Id_type Afid;
+	Id_type AuId = 0;
+	Id_type AfId = 0;
 };
 
 struct entity{
-	Id_type Id;
-	Id_type J_Id;
-	Id_type C_Id;
+	Id_type Id = 0;
+	Id_type J_Id = 0;
+	Id_type C_Id = 0;
 	std::vector<AA> AAs;
 	std::vector<Id_type> R_Id;
 	std::vector<Id_type> F_Id;
