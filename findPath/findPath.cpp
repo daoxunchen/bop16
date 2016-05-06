f0904c9d896af950c70529f7c4c1fa3e24288819
daoxunchen <acy631545607@gmail.com>
2016-05-06 14:59:09 +0800

    1-hop (not test yet)

--- findPath/findPath.cpp
+++ findPath/findPath.cpp
@@ -2,9 +2,13 @@
 #include "../Query.h"
 
 #include <string>
+#include <ctime>
+#include <algorithm>
 #include <thread>
 
 using namespace std;
+
+#define U(x) L ## x
 
 Entity_List startEntities, endEntities;
 vector<vector<Id_type>> res;	// final result
@@ -13,6 +17,8 @@ QueryAttri label[2];
 
 vector<vector<Id_type>> findPath(Id_type id1, Id_type id2)
 {
+	auto start_time = clock();
+
 	// clear All for a new process
 	res.clear();
 	startEntities.clear();
@@ -30,18 +36,36 @@ vector<vector<Id_type>> findPath(Id_type id1, Id_type id2)
 	//	find path process
 	if (label[0] == QueryAttri::Id) {
 		if (label[1] == QueryAttri::Id) {	//	Id-Id
-
+			//	1-hop
+			if (startEntities[0].R_Id.cend() != 
+				find(startEntities[0].R_Id.cbegin(), startEntities[0].R_Id.cend(), id2)) {
+				res.push_back(vector<Id_type>({ id1,id2 }));
+			}
+			//TODO: find 2 and 3
 		} else {	//	Id-Auid
-
+			//	1-hop
+			if (startEntities[0].AAs.cend() != 
+				find_if(startEntities[0].AAs.cbegin(), startEntities[0].AAs.cend(), 
+					[&](AA varA) {return varA.AuId == id2; })) {
+				res.push_back(vector<Id_type>({ id1,id2 }));
+			}
+			//TODO: find 2 and 3
 		}
 	} else {
 		if (label[1] == QueryAttri::Id) {	//	Auid-Id
-
+			//	1-hop
+			if (startEntities.cend() != 
+				find(startEntities.cbegin(), startEntities.cend(), endEntities[0])) {
+				res.push_back(vector<Id_type>({ id1,id2 }));
+			}
+			//TODO: find 2 and 3
 		} else {	//	Auid-Auid
-
+			//	no 1-hop
+			//TODO: find 2 and 3
 		}
 	}
 
-	//res.push_back(vector<Id_type>({ id1,id2 }));
+	auto end_time = clock();
+
 	return res;
 }
